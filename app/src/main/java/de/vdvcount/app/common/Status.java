3fa0fdd5fbfe5a2e1190b3bfca310621cd95e15a
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-08 18:26:53 +0200

    added Value presets for Status variables, references #2

--- app/src/main/java/de/vdvcount/app/common/Status.java
+++ app/src/main/java/de/vdvcount/app/common/Status.java
@@ -4,6 +4,11 @@ public class Status extends KeyValueStore {
 
     public final static String STATUS = "de.vdvcount.app.kvs.status.STATUS";
 
+    public class Values {
+        public final static String INITIAL = "INITIAL";
+        public final static String READY = "READY";
+    }
+
     static {
         Status.init("de.vdvcount.app.kvs.status");
     }
