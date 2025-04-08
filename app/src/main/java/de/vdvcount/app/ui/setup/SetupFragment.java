694adb3c8f1e68b9bcd77ecfa48a311f23c8147a
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-08 19:28:48 +0200

    added basic check for required permissions, references #6

--- app/src/main/java/de/vdvcount/app/ui/setup/SetupFragment.java
+++ app/src/main/java/de/vdvcount/app/ui/setup/SetupFragment.java
@@ -3,6 +3,8 @@ package de.vdvcount.app.ui.setup;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
+
+import java.util.ArrayList;
 
 import de.vdvcount.app.R;
 import de.vdvcount.app.databinding.FragmentSetupBinding;
@@ -28,7 +35,6 @@ public class SetupFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.viewModel = new ViewModelProvider(this).get(SetupViewModel.class);
     }
 
     @Nullable
@@ -37,9 +43,32 @@ public class SetupFragment extends Fragment {
         this.dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_setup, container, false);
         this.dataBinding.setLifecycleOwner(this.getViewLifecycleOwner());
 
-        this.viewModel.initAppilcation();
-
         return this.dataBinding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.viewModel = new ViewModelProvider(this).get(SetupViewModel.class);
+
+        this.dataBinding.btnContinue.setOnClickListener(sender -> {
+            String[] permissions = {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.CAMERA
+            };
+
+            Permissions.check(this.getContext(), permissions, null, null, new PermissionHandler() {
+                @Override
+                public void onGranted() {
+                    viewModel.initAppilcation();
+                }
+
+                @Override
+                public void onDenied(Context context, ArrayList<String> deniedPermissions) {
+                    super.onDenied(context, deniedPermissions);
+                }
+            });
+        });
+    }
 }
