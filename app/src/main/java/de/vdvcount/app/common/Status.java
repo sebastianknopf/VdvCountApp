40078d1fdc8c877b582fea9c51c76fff916441a2
sebastianknopf <mail@sebastian-knopf.de>
2025-04-13 11:23:31 +0200

    extended status class to provide support for storing last stop ID and stop name, references #4 and #11

--- app/src/main/java/de/vdvcount/app/common/Status.java
+++ app/src/main/java/de/vdvcount/app/common/Status.java
@@ -4,9 +4,13 @@ public class Status extends KeyValueStore {
 
     public final static String STATUS = "de.vdvcount.app.kvs.status.STATUS";
 
-    public class Values {
+    public final static String CURRENT_STOP_ID = "de.vdvcount.app.kvs.status.CURRENT_STOP_ID";
+    public final static String CURRENT_STOP_NAME = "de.vdvcount.app.kvs.status.CURRENT_STOP_NAME";
+
+    public static class Values {
         public final static String INITIAL = "INITIAL";
         public final static String READY = "READY";
+        public final static String COUNTING = "COUNTING";
     }
 
 }
