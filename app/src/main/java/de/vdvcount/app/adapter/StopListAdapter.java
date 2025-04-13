77d358b825c6184369dcd32715958d58f5eacfe4
sebastianknopf <mail@sebastian-knopf.de>
2025-04-13 09:59:23 +0200

    connected StopListAdapter and stop_item layout, references #4 and #11

--- app/src/main/java/de/vdvcount/app/adapter/StopListAdapter.java
+++ app/src/main/java/de/vdvcount/app/adapter/StopListAdapter.java
@@ -1,30 +1,34 @@
 package de.vdvcount.app.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import de.vdvcount.app.model.Departure;
+import de.vdvcount.app.R;
+import de.vdvcount.app.databinding.StopItemBinding;
+import de.vdvcount.app.model.Station;
 
 public class StopListAdapter extends RecyclerView.Adapter<StopListAdapter.ViewHolder> {
 
-    private List<Departure> departureList;
+    private List<Station> stationList;
 
     public StopListAdapter() {
-        this.departureList = new ArrayList<>();
+        this.stationList = new ArrayList<>();
     }
 
-    public StopListAdapter(List<Departure> departureList) {
-        this.departureList = departureList;
+    public StopListAdapter(List<Station> stationList) {
+        this.stationList = stationList;
     }
 
-    public void setDepartureList(List<Departure> departureList) {
-        this.departureList = departureList;
+    public void setStationList(List<Station> stationList) {
+        this.stationList = stationList;
         this.notifyDataSetChanged();
     }
 
@@ -37,38 +41,38 @@ public class StopListAdapter extends RecyclerView.Adapter<StopListAdapter.ViewHo
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        /*TicketItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.ticket_item, parent, false);
-        return new StopListAdapter.ViewHolder(parent.getContext(), binding);*/
+        StopItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.stop_item, parent, false);
+        return new StopListAdapter.ViewHolder(parent.getContext(), binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Departure obj = this.departureList.get(position);
+        Station obj = this.stationList.get(position);
         /*holder.setTicket(obj, position);
         holder.setOnManualValidationListener(this.manualValidationListener);*/
     }
 
     @Override
     public int getItemCount() {
-        return this.departureList.size();
+        return this.stationList.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         private final Context context;
-        private Departure departure;
-        //private final TicketItemBinding itemBinding;
+        private Station station;
+        private final StopItemBinding itemBinding;
         //private OnManualValidationListener manualValidationListener;
 
-        public ViewHolder(Context context/*, TicketItemBinding itemBinding*/) {
-            super(null);
+        public ViewHolder(Context context, StopItemBinding itemBinding) {
+            super(itemBinding.getRoot());
 
             this.context = context;
-            //this.itemBinding = itemBinding;
+            this.itemBinding = itemBinding;
         }
 
-        public void setDeparture(Departure departure, int index) {
-            this.departure = departure;
+        public void setDeparture(Station station, int index) {
+            this.station = station;
         }
 
         /*public void setOnManualValidationListener(OnManualValidationListener listener) {
