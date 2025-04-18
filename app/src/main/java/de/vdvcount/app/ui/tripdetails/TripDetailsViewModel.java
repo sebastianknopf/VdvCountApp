c7ec858a8cd2b9e2bc41bdd988c64d776ed38656
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-18 09:24:33 +0200

    added calls for storing / restoring data in TripDetailsFragment, references #4 and #12

--- app/src/main/java/de/vdvcount/app/ui/tripdetails/TripDetailsViewModel.java
+++ app/src/main/java/de/vdvcount/app/ui/tripdetails/TripDetailsViewModel.java
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import de.vdvcount.app.common.Status;
+import de.vdvcount.app.filesystem.FilesystemRepository;
 import de.vdvcount.app.model.CountedTrip;
+import de.vdvcount.app.model.Trip;
+import de.vdvcount.app.remote.RemoteRepository;
 
 public class TripDetailsViewModel extends ViewModel {
 
@@ -19,19 +22,51 @@ public class TripDetailsViewModel extends ViewModel {
     }
 
     public void startCountedTrip(int tripId, int startStopSequence) {
-        Status.setString(Status.STATUS, Status.Values.COUNTING);
-        Status.setInt(Status.CURRENT_TRIP_ID, tripId);
-        Status.setInt(Status.CURRENT_START_STOP_SEQUENCE, startStopSequence);
+        Runnable runnable = () -> {
+            RemoteRepository remoteRepository = RemoteRepository.getInstance();
+            Trip trip = remoteRepository.getTripByTripId(tripId);
+
+            FilesystemRepository filesystemRepository = FilesystemRepository.getInstance();
+            CountedTrip countedTrip = filesystemRepository.startCountedTrip(trip);
+
+            this.countedTrip.postValue(countedTrip);
+
+            Status.setString(Status.STATUS, Status.Values.COUNTING);
+            Status.setInt(Status.CURRENT_TRIP_ID, tripId);
+            Status.setInt(Status.CURRENT_START_STOP_SEQUENCE, startStopSequence);
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
     public void loadCountedTrip() {
+        Runnable runnable = () -> {
+            FilesystemRepository repository = FilesystemRepository.getInstance();
+            this.countedTrip.postValue(repository.loadCountedTrip());
+        };
 
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
     public void closeCountedTrip() {
-        Status.setString(Status.STATUS, Status.Values.READY);
-        Status.setInt(Status.CURRENT_TRIP_ID, -1);
-        Status.setInt(Status.CURRENT_START_STOP_SEQUENCE, -1);
+        Runnable runnable = () -> {
+            FilesystemRepository filesystemRepository = FilesystemRepository.getInstance();
+            CountedTrip countedTrip = filesystemRepository.loadCountedTrip();
+
+            RemoteRepository remoteRepository = RemoteRepository.getInstance();
+            remoteRepository.postResults(countedTrip);
+
+            filesystemRepository.closeCountedTrip();
+
+            Status.setString(Status.STATUS, Status.Values.READY);
+            Status.setInt(Status.CURRENT_TRIP_ID, -1);
+            Status.setInt(Status.CURRENT_START_STOP_SEQUENCE, -1);
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
 }
