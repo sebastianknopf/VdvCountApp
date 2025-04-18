6e5b3387238130931724e858b287d3d82a7b103d
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-18 09:07:15 +0200

    added access methods to FilesystemRepository, references #15

--- app/src/main/java/de/vdvcount/app/filesystem/FilesystemRepository.java
+++ app/src/main/java/de/vdvcount/app/filesystem/FilesystemRepository.java
@@ -1,8 +1,19 @@
 package de.vdvcount.app.filesystem;
 
+import com.google.gson.Gson;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 import de.vdvcount.app.App;
+import de.vdvcount.app.model.CountedTrip;
+import de.vdvcount.app.model.Trip;
+import de.vdvcount.app.remote.CountedTripObject;
+import kotlin.text.Charsets;
 
 public class FilesystemRepository {
 
@@ -24,6 +35,87 @@ public class FilesystemRepository {
         this.verifyFileSystemStructure();
     }
 
+    public CountedTrip startCountedTrip(Trip trip) {
+        CountedTrip countedTrip = new CountedTrip();
+        countedTrip.setTripId(trip.getTripId());
+        countedTrip.setLine(trip.getLine());
+        countedTrip.setDirection(trip.getDirection());
+        countedTrip.setHeadsign(trip.getHeadsign());
+        countedTrip.setInternationalId(trip.getInternationalId());
+        countedTrip.setNextTripId(trip.getNextTripId());
+
+        this.updateCountedTrip(countedTrip);
+
+        return countedTrip;
+    }
+
+    public void updateCountedTrip(CountedTrip countedTrip) {
+        String countedTripFilename = this.getCountedTripFilename();
+
+        Gson gson = new Gson();
+        String countedTripData = gson.toJson(countedTrip.mapApiObject());
+
+        FileOutputStream fos = null;
+
+        try {
+            File outputFile = new File(countedTripFilename);
+            outputFile.createNewFile();
+
+            fos = new FileOutputStream(countedTripFilename);
+
+            PrintStream printStream = new PrintStream(fos);
+            printStream.print(countedTripData);
+        } catch (IOException e) {
+
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    public CountedTrip loadCountedTrip() {
+        String countedTripFilename = this.getCountedTripFilename();
+        File file = new File(countedTripFilename);
+
+        if (!file.exists()) {
+            return null;
+        }
+
+        byte[] buffer = new byte[(int) file.length()];
+        InputStream ios = null;
+
+        try {
+            ios = new FileInputStream(file);
+            ios.read(buffer);
+        } catch(IOException e) {
+            return null;
+        } finally {
+            try {
+                if (ios != null) {
+                    ios.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        String inspectionObjectJson = new String(buffer, Charsets.UTF_8);
+        Gson gson = new Gson();
+
+        CountedTripObject countedTripObject = gson.fromJson(inspectionObjectJson, CountedTripObject.class);
+
+        return countedTripObject.mapDomainModel();
+    }
+
+    public void closeCountedTrip() {
+        String countedTripFilename = this.getCountedTripFilename();
+        File file = new File(countedTripFilename);
+        file.delete();
+    }
+
     private void verifyFileSystemStructure() {
         File file = new File(this.getAppDataPath());
         if (!file.isDirectory()) {
@@ -38,5 +130,14 @@ public class FilesystemRepository {
         builder.append("appdata");
 
         return builder.toString();
+    }
+
+    private String getCountedTripFilename() {
+        StringBuilder fileNameBuilder = new StringBuilder();
+        fileNameBuilder.append(this.getAppDataPath());
+        fileNameBuilder.append(File.separator);
+        fileNameBuilder.append("countedTrip.json");
+
+        return fileNameBuilder.toString();
     }
 }
