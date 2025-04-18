fce9f2bdbc98af6a4c78b87d255f543e0a9ff9bc
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-18 09:07:39 +0200

    extended CountedTripObject to map domain model correctly, references #15

--- app/src/main/java/de/vdvcount/app/remote/CountedTripObject.java
+++ app/src/main/java/de/vdvcount/app/remote/CountedTripObject.java
@@ -1,4 +1,20 @@
 package de.vdvcount.app.remote;
 
+import de.vdvcount.app.model.CountedTrip;
+
 public class CountedTripObject extends TripObject {
+
+    @Override
+    public CountedTrip mapDomainModel() {
+        CountedTrip domainModel = new CountedTrip();
+
+        domainModel.setTripId(this.getTripId());
+        domainModel.setLine(this.getLine().mapDomainModel());
+        domainModel.setDirection(this.getDirection());
+        domainModel.setHeadsign(this.getHeadsign());
+        domainModel.setInternationalId(this.getInternationalId());
+        domainModel.setNextTripId(this.getNextTripId());
+
+        return domainModel;
+    }
 }
