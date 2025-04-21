c17a2e0e252ba644e455b9201b417845346d9219
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-04-21 07:29:07 +0200

    added basic CountingFragment, references #4 and #13

---
+++ app/src/main/java/de/vdvcount/app/ui/counting/CountingFragment.java
@@ -0,0 +1,76 @@
+package de.vdvcount.app.ui.counting;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.NavController;
+import de.vdvcount.app.AppActivity;
+import de.vdvcount.app.R;
+import de.vdvcount.app.databinding.FragmentCountingBinding;
+import de.vdvcount.app.ui.departure.DepartureViewModel;
+
+public class CountingFragment extends Fragment {
+
+    private FragmentCountingBinding dataBinding;
+    private CountingViewModel viewModel;
+
+    private NavController navigationController;
+
+    public static CountingFragment newInstance() {
+        return new CountingFragment();
+    }
+
+    public CountingFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_departure, container, false);
+        this.dataBinding.setLifecycleOwner(this.getViewLifecycleOwner());
+
+        return this.dataBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.viewModel = new ViewModelProvider(this).get(CountingViewModel.class);
+
+        this.initViewEvents();
+        this.initObserverEvents();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        AppActivity appActivity = (AppActivity) this.getActivity();
+        appActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        appActivity.setTitle(R.string.counting_title);
+
+        this.setHasOptionsMenu(true);
+
+        this.navigationController = appActivity.getNavigationController();
+    }
+
+    private void initViewEvents() {
+
+    }
+
+    private void initObserverEvents() {
+
+    }
+
+}
---
+++ app/src/main/java/de/vdvcount/app/ui/counting/CountingViewModel.java
@@ -0,0 +1,7 @@
+package de.vdvcount.app.ui.counting;
+
+import androidx.lifecycle.ViewModel;
+
+public class CountingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
