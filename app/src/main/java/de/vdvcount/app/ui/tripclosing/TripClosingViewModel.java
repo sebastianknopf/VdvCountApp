989f83646a11699112593add323b33b702addc28
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-06-04 08:20:54 +0200

    enhanced TripClosingViewModel to store stay-in-vehicle status props, references #21

--- app/src/main/java/de/vdvcount/app/ui/tripclosing/TripClosingViewModel.java
+++ app/src/main/java/de/vdvcount/app/ui/tripclosing/TripClosingViewModel.java
@@ -6,7 +6,10 @@ import androidx.lifecycle.ViewModel;
 
 import de.vdvcount.app.common.Status;
 import de.vdvcount.app.filesystem.FilesystemRepository;
+import de.vdvcount.app.model.CountedStopTime;
 import de.vdvcount.app.model.CountedTrip;
+import de.vdvcount.app.model.CountingSequence;
+import de.vdvcount.app.model.PassengerCountingEvent;
 import de.vdvcount.app.remote.RemoteRepository;
 
 public class TripClosingViewModel extends ViewModel {
@@ -28,6 +31,17 @@ public class TripClosingViewModel extends ViewModel {
             FilesystemRepository filesystemRepository = FilesystemRepository.getInstance();
             CountedTrip countedTrip = filesystemRepository.loadCountedTrip();
 
+            if (stayInVehicle) {
+                PassengerCountingEvent passengerCountingEvent = this.extractLastPce(countedTrip);
+                Status.setString(Status.LAST_PCE, passengerCountingEvent != null ? passengerCountingEvent.serialize() : null);
+
+                if (passengerCountingEvent != null) {
+                    for (CountingSequence cs : passengerCountingEvent.getCountingSequences()) {
+                        cs.setIn(0);
+                    }
+                }
+            }
+
             RemoteRepository remoteRepository = RemoteRepository.getInstance();
             if (remoteRepository.postResults(countedTrip)) {
                 filesystemRepository.closeCountedTrip();
@@ -38,6 +52,11 @@ public class TripClosingViewModel extends ViewModel {
                 Status.setInt(Status.CURRENT_TRIP_ID, -1);
                 Status.setInt(Status.CURRENT_START_STOP_SEQUENCE, -1);
                 Status.setString(Status.CURRENT_VEHICLE_ID, "");
+
+                if (stayInVehicle) {
+                    Status.setString(Status.LAST_VEHICLE_ID, countedTrip.getVehicleId());
+                    Status.setBoolean(Status.STAY_IN_VEHICLE, false);
+                }
             } else {
                 this.state.postValue(TripClosingFragment.State.ERROR);
             }
@@ -45,5 +64,16 @@ public class TripClosingViewModel extends ViewModel {
 
         Thread thread = new Thread(runnable);
         thread.start();
+    }
+
+    private PassengerCountingEvent extractLastPce(CountedTrip trip) {
+        CountedStopTime lastCountedStopTime = trip.getCountedStopTimes().get(trip.getCountedStopTimes().size() - 1);
+
+        PassengerCountingEvent lastPassengerCountingEvent = null;
+        if (!lastCountedStopTime.getPassengerCountingEvents().isEmpty()) {
+            lastPassengerCountingEvent = lastCountedStopTime.getPassengerCountingEvents().get(lastCountedStopTime.getPassengerCountingEvents().size() - 1);
+        }
+
+        return lastPassengerCountingEvent;
     }
 }
